bddfff1ce27480cc933eb1508d685715bad64f08
JeanDevFR <castellotti.j@gmail.com>
2022-03-18 12:44:26 +0100

    Create establishments migration

---
+++ database/migrations/1647589842936_establishments.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Establishments extends BaseSchema {
+  protected tableName = 'establishments'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name').notNullable()
+      table.text('description').notNullable()
+      table.string('city').notNullable()
+      table.string('address').notNullable()
+      table.string('hero').notNullable()
+      table.integer('user_id').unsigned().references('users.id')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
