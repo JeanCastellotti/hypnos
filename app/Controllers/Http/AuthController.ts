dde6878e37241577b34ad8a003eff128050fba6a
JeanDevFR <castellotti.j@gmail.com>
2022-03-19 08:16:00 +0100

    Create login method

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,3 +1,16 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import LoginValidator from 'App/Validators/LoginValidator'
 
-export default class AuthController {}
+export default class AuthController {
+  public async login({ request, response, auth, session }: HttpContextContract) {
+    const { email, password } = await request.validate(LoginValidator)
+    try {
+      await auth.attempt(email, password)
+    } catch (error) {
+      session.flash('error', 'Un problème est survenu')
+    }
+
+    session.flash('succes', 'Vous êtes connecté')
+    return response.redirect('/dasboard')
+  }
+}
