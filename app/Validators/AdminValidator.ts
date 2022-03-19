1dd3b683bdb7a82a8dca8b484b4983b19af01a86
JeanDevFR <castellotti.j@gmail.com>
2022-03-19 07:58:35 +0100

    Add Admin Validator

---
+++ app/Validators/AdminValidator.ts
@@ -0,0 +1,56 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class AdminValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  /*
+   * Define schema to validate the "shape", "type", "formatting" and "integrity" of data.
+   *
+   * For example:
+   * 1. The username must be of data type string. But then also, it should
+   *    not contain special characters or numbers.
+   *    ```
+   *     schema.string({}, [ rules.alpha() ])
+   *    ```
+   *
+   * 2. The email must be of data type string, formatted as a valid
+   *    email. But also, not used by any other user.
+   *    ```
+   *     schema.string({}, [
+   *       rules.email(),
+   *       rules.unique({ table: 'users', column: 'email' }),
+   *     ])
+   *    ```
+   */
+  public schema = schema.create({
+    lastname: schema.string({ trim: true }, [rules.alpha()]),
+    firstname: schema.string({ trim: true }, [rules.alpha()]),
+    email: schema.string({ trim: true }, [
+      rules.email(),
+      rules.unique({ table: 'users', column: 'email' }),
+    ]),
+    password: schema.string({}, [rules.confirmed()]),
+  })
+
+  /**
+   * Custom messages for validation failures. You can make use of dot notation `(.)`
+   * for targeting nested fields and array expressions `(*)` for targeting all
+   * children of an array. For example:
+   *
+   * {
+   *   'profile.username.required': 'Username is required',
+   *   'scores.*.number': 'Define scores as valid numbers'
+   * }
+   *
+   */
+  public messages = {
+    'lastname.required': 'Le nom est obligatoire',
+    'firstname.required': 'Le prénom est obligatoire',
+    'email.required': "L'adresse email est obligatoire",
+    'email.email': "L'adresse email n'est pas valide",
+    'email.unique': "L'adresse email n'est pas disponible",
+    'password.required': 'Le mot de passe est obligatoire',
+    'confirmed': 'Les mots de passe ne correspondent pas',
+  }
+}
