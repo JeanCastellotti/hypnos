71fbea7119c52d56ea1d824c698f3d94c3d209e2
JeanDevFR <castellotti.j@gmail.com>
2022-03-20 16:43:16 +0100

    Create ManagerUpdate Validator

---
+++ app/Validators/ManagerUpdateValidator.ts
@@ -0,0 +1,28 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class ManagerValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    lastname: schema.string.optional({ trim: true }, [rules.alpha()]),
+    firstname: schema.string.optional({ trim: true }, [rules.alpha()]),
+    email: schema.string.optional({ trim: true }, [
+      rules.email(),
+      rules.unique({ table: 'users', column: 'email', whereNot: { id: this.ctx.params.id } }),
+    ]),
+    password: schema.string.optional({}, [rules.confirmed()]),
+  })
+
+  public messages = {
+    'lastname.required': 'Le nom est obligatoire',
+    'lastname.alpha': 'Le nom ne peut contenir que des lettres',
+    'firstname.required': 'Le prénom est obligatoire',
+    'firstname.alpha': 'Le prénom ne peut contenir que des lettres',
+    'email.required': "L'adresse email est obligatoire",
+    'email.email': "L'adresse email est incorrecte",
+    'email.unique': "L'adresse email n'est pas disponible",
+    'password.required': 'Le mot de passe est obligatoire',
+    'confirmed': 'Les mots de passe ne correspondent pas',
+  }
+}
