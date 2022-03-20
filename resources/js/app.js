9fc626652313dbfeb9b3f0dbc025776ce31c1c17
JeanDevFR <castellotti.j@gmail.com>
2022-03-20 22:08:56 +0100

    Add modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
+import Alpine from 'alpinejs'
 import '../css/app.css'
+
+Alpine.data('modal', () => ({
+  isOpen: false,
+  message: null,
+  id: null,
+  open(elt) {
+    this.isOpen = true
+    this.message = elt.dataset.message
+    this.id = elt.dataset.id
+  },
+  close() {
+    this.isOpen = false
+  },
+}))
+
+Alpine.start()
