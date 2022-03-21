34873786b78cd349f2f3109dc81ba44b254c271a
JeanDevFR <castellotti.j@gmail.com>
2022-03-21 12:47:58 +0100

    Create suites et pictures migrations

---
+++ database/migrations/1647861927044_suites.ts
@@ -0,0 +1,26 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Suites extends BaseSchema {
+  protected tableName = 'suites'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('title').notNullable()
+      table.text('description').notNullable()
+      table.float('price').notNullable()
+      table.string('booking_url').notNullable()
+      table
+        .integer('establishment_id')
+        .references('establishments.id')
+        .onDelete('CASCADE')
+        .notNullable()
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
