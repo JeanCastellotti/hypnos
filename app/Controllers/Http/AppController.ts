20526a4341546f0fb3e802d25246642c6407759c
JeanDevFR <castellotti.j@gmail.com>
2022-03-22 21:57:05 +0100

    Update createAdmin method

--- app/Controllers/Http/AppController.ts
+++ app/Controllers/Http/AppController.ts
@@ -1,13 +1,14 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Establishment from 'App/Models/Establishment'
 import User from 'App/Models/User'
 import AdminValidator from 'App/Validators/AdminValidator'
 
 export default class AppsController {
-  public async showAdmin({ response, view }: HttpContextContract) {
+  public async createAdmin({ response, view }: HttpContextContract) {
     const admin = await User.findBy('role', 'admin')
 
     if (admin) {
-      return response.redirect('/')
+      return response.redirect().toRoute('home')
     }
 
     return view.render('admin/index')
@@ -20,6 +21,7 @@ export default class AppsController {
       await User.create({ ...data, role: 'admin' })
     } catch (error) {
       session.flash('error', "Un problème est survenu lors de la création de l'administrateur")
+      return response.redirect().back()
     }
 
     session.flash('success', "L'administrateur a bien été créé")
