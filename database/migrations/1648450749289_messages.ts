eeca0e3688d5544fd1a1a03bdb8c275b284499fd
JeanDevFR <castellotti.j@gmail.com>
2022-03-28 09:04:38 +0200

    Create messages migration

---
+++ database/migrations/1648450749289_messages.ts
@@ -0,0 +1,21 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Messages extends BaseSchema {
+  protected tableName = 'messages'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('firstname').notNullable()
+      table.string('lastname').notNullable()
+      table.string('email').notNullable()
+      table.string('subject').notNullable()
+      table.text('message').notNullable()
+      table.timestamp('created_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
