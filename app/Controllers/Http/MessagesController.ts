9bc2370c15a461f6dcb972d5ae154c3753e6eadc
JeanDevFR <castellotti.j@gmail.com>
2022-03-29 17:26:08 +0200

    Create messages controller

---
+++ app/Controllers/Http/MessagesController.ts
@@ -0,0 +1,28 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Message from 'App/Models/Message'
+import ContactValidator from 'App/Validators/ContactValidator'
+
+export default class MessagesController {
+  public async index({ view }: HttpContextContract) {
+    const messages = await Message.all()
+    return view.render('pages/dashboard/messages/index', { messages })
+  }
+
+  public async create({ view }: HttpContextContract) {
+    return view.render('pages/messages/create')
+  }
+
+  public async store({ request, response, session }: HttpContextContract) {
+    const data = await request.validate(ContactValidator)
+
+    try {
+      await Message.create({ ...data })
+    } catch (error) {
+      session.flash('error', "Un problème est survenu lors de l'enregistrement du message")
+      return response.redirect().back()
+    }
+
+    session.flash('success', 'Votre message a bien été envoyé')
+    return response.redirect().toRoute('app.home')
+  }
+}
