705529e7f9ed4b93b252b0d9a67b28f974d27701
JeanDevFR <castellotti.j@gmail.com>
2022-03-29 17:22:28 +0200

    Create Contact Validator

---
+++ app/Validators/ContactValidator.ts
@@ -0,0 +1,23 @@
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class ContactValidator {
+  constructor(protected ctx: HttpContextContract) {}
+
+  public schema = schema.create({
+    subject: schema.string(),
+    lastname: schema.string({ trim: true }, [rules.alpha()]),
+    firstname: schema.string({ trim: true }, [rules.alpha()]),
+    email: schema.string({ trim: true }, [rules.email()]),
+    message: schema.string({ trim: true }),
+  })
+
+  public messages = {
+    'subject.required': 'Le sujet est obligatoire',
+    'lastname.required': 'Le nom est obligatoire',
+    'firstname.required': 'Le prénom est obligatoire',
+    'email.required': "L'adresse email est obligatoire",
+    'email.email': "L'adresse email est invalide",
+    'message.required': 'Le message est obligatoire',
+  }
+}
