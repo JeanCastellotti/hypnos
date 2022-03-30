2e29543f3d171467fd1c36bffbb884f0660cdf76
JeanDevFR <castellotti.j@gmail.com>
2022-03-30 16:11:52 +0200

    Remove useless log

--- resources/js/components/bookingForm.js
+++ resources/js/components/bookingForm.js
@@ -1,6 +1,5 @@
 export default () => ({
   updateQuery() {
-    console.log(this.$el)
     const url = new URL(window.location)
     url.searchParams.set(this.$el.name, this.$el.value)
     if (this.$el.name === 'establishment') url.searchParams.delete('suite')
