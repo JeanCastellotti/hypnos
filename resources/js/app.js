cd2e600cefe2dc73010b15bed90eb24558d2fd99
JeanDevFR <castellotti.j@gmail.com>
2022-03-30 16:11:40 +0200

    Remove alpinejs modal and snackbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,12 @@
 import '../css/app.css'
 
 import Alpine from 'alpinejs'
-import modal from './components/modal'
-import snackbar from './components/snackbar'
+// import modal from './components/modal'
+// import snackbar from './components/snackbar'
 import bookingForm from './components/bookingForm'
 
-Alpine.data('modal', modal)
-Alpine.data('snackbar', snackbar)
+// Alpine.data('modal', modal)
+// Alpine.data('snackbar', snackbar)
 Alpine.data('bookingForm', bookingForm)
 
 Alpine.start()
