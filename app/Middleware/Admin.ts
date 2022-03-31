5e045c9f6766130255f081f8bad7e88f8a0e9445
JeanDevFR <castellotti.j@gmail.com>
2022-03-31 15:33:49 +0200

    Update Admin Middleware

--- app/Middleware/Admin.ts
+++ app/Middleware/Admin.ts
@@ -4,9 +4,7 @@ export default class Admin {
   public async handle({ request, auth, response }: HttpContextContract, next: () => Promise<void>) {
     console.log(request.url(), request.method())
     if (auth.user?.role !== 'admin') {
-      console.log(response)
-
-      // return response.redirect().back()
+      return response.redirect().back()
     }
     await next()
   }
