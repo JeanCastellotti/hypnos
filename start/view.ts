d5edbfc4c759ee335b0cfd3942bc69e0d1ae581a
JeanDevFR <castellotti.j@gmail.com>
2022-04-07 16:56:59 +0200

    Create view preload file

---
+++ start/view.ts
@@ -0,0 +1,14 @@
+/*
+|--------------------------------------------------------------------------
+| Preloaded File
+|--------------------------------------------------------------------------
+|
+| Any code written inside this file will be executed during the application
+| boot.
+|
+*/
+
+import View from '@ioc:Adonis/Core/View'
+import Drive from '@ioc:Adonis/Core/Drive'
+
+View.global('Drive', Drive)
