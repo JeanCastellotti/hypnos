3e5039e341d694840d8821031d7ae4aadb76cbfa
JeanDevFR <castellotti.j@gmail.com>
2022-04-07 17:02:28 +0200

    Update suites controller

--- app/Controllers/Http/Dashboard/SuitesController.ts
+++ app/Controllers/Http/Dashboard/SuitesController.ts
@@ -4,24 +4,22 @@ import SuiteValidator from 'App/Validators/SuiteValidator'
 import { string } from '@ioc:Adonis/Core/Helpers'
 import Drive from '@ioc:Adonis/Core/Drive'
 import SuiteUpdateValidator from 'App/Validators/SuiteUpdateValidator'
-import Establishment from 'App/Models/Establishment'
-import Role from 'App/Enums/Roles'
 import sharp from 'sharp'
+import SuitesPicture from 'App/Models/SuitesPicture'
 
 export default class SuitesController {
   public async index({ view, bouncer, auth }: HttpContextContract) {
     await bouncer.with('DashboardPolicy').authorize('viewSuites')
-    const establishment = await Establishment.query().where('user_id', auth.user!.id).first()
-    const suites = await establishment?.related('suites').query()
-    return view.render('pages/dashboard/suites/index', { suites })
+    await auth.user!.load('establishment')
+    const suites = await auth.user!.establishment?.related('suites').query()
+    return view.render('dashboard/suites/index', { suites })
   }
 
   public async create({ view, response, session, auth, bouncer }: HttpContextContract) {
     await bouncer.with('DashboardPolicy').authorize('createSuite')
     const establishment = await auth.user!.related('establishment').query().first()
-    const establishments = await Establishment.all()
 
-    if (!establishment && auth.user!.role !== Role.ADMIN) {
+    if (!establishment) {
       session.flash(
         'error',
         "Vous n'avez pas encore la possibilité de créer une suite. Si le problème persiste, contactez l'administrateur."
@@ -29,37 +27,42 @@ export default class SuitesController {
       return response.redirect().back()
     }
 
-    return view.render('pages/dashboard/suites/create', { establishments })
+    return view.render('dashboard/suites/create')
   }
 
-  public async store({ request, response, session, auth, bouncer }) {
+  public async store({ request, response, session, auth, bouncer }: HttpContextContract) {
     await bouncer.with('DashboardPolicy').authorize('storeSuite')
     const { picture1, picture2, ...data } = await request.validate(SuiteValidator)
-    const filename1 = string.generateRandom(32)
-    const filename2 = string.generateRandom(32)
-    const establishment = await auth.user.related('establishment').query().first()
+    await auth.user!.load('establishment')
 
     try {
+      const pictures = await SuitesPicture.createMany([
+        {
+          filename: string.generateRandom(32),
+          extname: picture1.extname,
+        },
+        {
+          filename: string.generateRandom(32),
+          extname: picture2.extname,
+        },
+      ])
+
       await Suite.create({
         ...data,
-        establishmentId: establishment.id,
-        picture_1: filename1 + '.' + picture1.extname,
-        picture_2: filename2 + '.' + picture2.extname,
+        establishmentId: auth.user!.establishment.id,
+        picture_1: pictures[0].id,
+        picture_2: pictures[1].id,
       })
-      // await picture1.moveToDisk('./', { name: filename1 })
-      // await picture2.moveToDisk('./', { name: filename2 })
+
       const resizePicture1Large = await sharp(picture1.tmpPath).resize(1280).toBuffer()
-      const resizePicture1Medium = await sharp(picture1.tmpPath).resize(768).toBuffer()
       const resizePicture1Small = await sharp(picture1.tmpPath).resize(640).toBuffer()
       const resizePicture2Large = await sharp(picture2.tmpPath).resize(1280).toBuffer()
-      const resizePicture2Medium = await sharp(picture2.tmpPath).resize(768).toBuffer()
       const resizePicture2Small = await sharp(picture2.tmpPath).resize(640).toBuffer()
-      await Drive.put(`${filename1}-large.${picture1.extname}`, resizePicture1Large)
-      await Drive.put(`${filename1}-medium.${picture1.extname}`, resizePicture1Medium)
-      await Drive.put(`${filename1}-small.${picture1.extname}`, resizePicture1Small)
-      await Drive.put(`${filename2}-large.${picture2.extname}`, resizePicture2Large)
-      await Drive.put(`${filename2}-medium.${picture2.extname}`, resizePicture2Medium)
-      await Drive.put(`${filename2}-small.${picture2.extname}`, resizePicture2Small)
+
+      await Drive.put(`${pictures[0].filename}-lg.${pictures[0].extname}`, resizePicture1Large)
+      await Drive.put(`${pictures[0].filename}-sm.${pictures[0].extname}`, resizePicture1Small)
+      await Drive.put(`${pictures[1].filename}-lg.${pictures[1].extname}`, resizePicture2Large)
+      await Drive.put(`${pictures[1].filename}-sm.${pictures[1].extname}`, resizePicture2Small)
     } catch (error) {
       session.flash('error', 'Un problème est survenu lors de la création de la suite')
       return response.redirect().back()
@@ -74,33 +77,53 @@ export default class SuitesController {
 
     await bouncer.with('DashboardPolicy').authorize('editSuite', suite)
 
-    return view.render('pages/dashboard/suites/edit', { suite })
+    return view.render('dashboard/suites/edit', { suite })
   }
 
   public async update({ request, response, params, session, bouncer }: HttpContextContract) {
     const { picture1, picture2, ...data } = await request.validate(SuiteUpdateValidator)
-
     const suite = await Suite.findOrFail(params.id)
-
     await bouncer.with('DashboardPolicy').authorize('updateSuite', suite)
 
     try {
       if (picture1) {
-        const filename = string.generateRandom(32) + '.' + picture1.extname
-        await Drive.delete(suite.picture_1)
-        await picture1.moveToDisk('./', { name: filename })
-        suite.picture_1 = filename
+        await suite.load('picture1')
+
+        await Drive.delete(`${suite.picture1.filename}-sm.${suite.picture1.extname}`)
+        await Drive.delete(`${suite.picture1.filename}-lg.${suite.picture1.extname}`)
+
+        const resizePictureLarge = await sharp(picture1.tmpPath).resize(1280).toBuffer()
+        const resizePictureSmall = await sharp(picture1.tmpPath).resize(640).toBuffer()
+
+        const filename = string.generateRandom(32)
+
+        await Drive.put(`${filename}-sm.${picture1.extname}`, resizePictureSmall)
+        await Drive.put(`${filename}-lg.${picture1.extname}`, resizePictureLarge)
+
+        suite.picture1.merge({ filename, extname: picture1.extname }).save()
       }
 
       if (picture2) {
-        const filename = string.generateRandom(32) + '.' + picture2.extname
-        await Drive.delete(suite.picture_2)
-        await picture2.moveToDisk('./', { name: filename })
-        suite.picture_2 = filename
+        await suite.load('picture2')
+
+        await Drive.delete(`${suite.picture2.filename}-sm.${suite.picture2.extname}`)
+        await Drive.delete(`${suite.picture2.filename}-lg.${suite.picture2.extname}`)
+
+        const resizePictureLarge = await sharp(picture2.tmpPath).resize(1280).toBuffer()
+        const resizePictureSmall = await sharp(picture2.tmpPath).resize(640).toBuffer()
+
+        const filename = string.generateRandom(32)
+
+        await Drive.put(`${filename}-sm.${picture2.extname}`, resizePictureSmall)
+        await Drive.put(`${filename}-lg.${picture2.extname}`, resizePictureLarge)
+
+        suite.picture2.merge({ filename, extname: picture2.extname }).save()
       }
 
       await suite.merge({ ...data }).save()
     } catch (error) {
+      console.log(error)
+
       session.flash('error', 'Un problème est survenu lors de la modification de la suite')
       return response.redirect().back()
     }
@@ -114,7 +137,7 @@ export default class SuitesController {
 
     await bouncer.with('DashboardPolicy').authorize('deleteSuite', suite)
 
-    return view.render('pages/dashboard/suites/delete', { suite })
+    return view.render('dashboard/suites/delete', { suite })
   }
 
   public async destroy({ params, response, session, bouncer }: HttpContextContract) {
