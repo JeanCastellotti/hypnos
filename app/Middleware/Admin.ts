5c8dbb9143355e85df6be6c81f7ed38aea0c447d
JeanDevFR <castellotti.j@gmail.com>
2022-04-10 14:38:07 +0200

    Update admin middleware

--- app/Middleware/Admin.ts
+++ app/Middleware/Admin.ts
@@ -1,8 +1,9 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Role from 'App/Enums/Roles'
 
 export default class Admin {
   public async handle({ auth, response }: HttpContextContract, next: () => Promise<void>) {
-    if (auth.user?.role !== 'admin') {
+    if (auth.user?.roleId !== Role.ADMIN) {
       return response.redirect().back()
     }
     await next()
