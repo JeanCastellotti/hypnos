d25b0fb7fa1b98fd1541872806f46833b3ae4b20
JeanDevFR <castellotti.j@gmail.com>
2022-04-10 14:38:14 +0200

    Update manager middlewar

--- app/Middleware/Manager.ts
+++ app/Middleware/Manager.ts
@@ -1,8 +1,9 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Role from 'App/Enums/Roles'
 
 export default class Manager {
   public async handle({ auth, response }: HttpContextContract, next: () => Promise<void>) {
-    if (auth.user?.role !== 'manager') {
+    if (auth.user?.roleId !== Role.MANAGER) {
       return response.redirect().back()
     }
     await next()
