07b2383239786d6b319db58a956448b73755ad37
JeanDevFR <castellotti.j@gmail.com>
2022-04-10 18:40:55 +0200

    Update admin validator

--- app/Validators/AdminValidator.ts
+++ app/Validators/AdminValidator.ts
@@ -11,7 +11,7 @@ export default class AdminValidator {
       rules.email(),
       rules.unique({ table: 'users', column: 'email' }),
     ]),
-    password: schema.string({}, [rules.confirmed()]),
+    password: schema.string({}, [rules.confirmed(), rules.minLength(8)]),
   })
 
   public messages = {
@@ -21,6 +21,7 @@ export default class AdminValidator {
     'email.email': "L'adresse email n'est pas valide",
     'email.unique': "L'adresse email n'est pas disponible",
     'password.required': 'Le mot de passe est obligatoire',
+    'password.minLength': 'Le mot de passe doit faire au moins 8 caractères',
     'confirmed': 'Les mots de passe ne correspondent pas',
   }
 }
