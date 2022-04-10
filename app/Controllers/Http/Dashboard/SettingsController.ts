8cbb8554aef556705cbbeb083e3a591d74f8ef04
JeanDevFR <castellotti.j@gmail.com>
2022-04-10 16:49:09 +0200

    Add update method

--- app/Controllers/Http/Dashboard/SettingsController.ts
+++ app/Controllers/Http/Dashboard/SettingsController.ts
@@ -1,8 +1,19 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+import UserValidator from 'App/Validators/UserValidator'
 
 export default class SettingsController {
   public async index({ view, bouncer }: HttpContextContract) {
     await bouncer.with('DashboardPolicy').authorize('viewSettings')
     return view.render('dashboard/settings/index')
+  }
+
+  public async update({ request, response, session, auth, bouncer }: HttpContextContract) {
+    await bouncer.with('DashboardPolicy').authorize('updateSettings')
+    const data = await request.validate(UserValidator)
+    const user = await User.findOrFail(auth.user!.id)
+    await user.merge(data).save()
+    session.flash('success', 'Votre compte a été mise à jour')
+    return response.redirect().toRoute('dashboard.settings.index')
   }
 }
